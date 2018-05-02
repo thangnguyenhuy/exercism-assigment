fb05ad2e2ffeeb2ef8b88156098b3c9291c5ba43
Thang Nguyen <thang@tinypulse.com>
2018-05-02 23:24:35 +0700

    using recursion for roman numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,29 +1,25 @@
 class Fixnum
-  
+  ROMAN_MAPPING = {
+    1000  => 'M',
+    900   => 'CM',
+    500   => 'D',
+    400   => 'CD',
+    100   => 'C',
+    90    => 'XC',
+    50    => 'L',
+    40    => 'XL',
+    10    => 'X',
+    9     => 'IX',
+    5     => 'V',
+    4     => 'IV',
+    1     => 'I'
+  }
+
   def to_roman
-    ronan_mapping = {
-      1000  => 'M',
-      900   => 'CM',
-      500   => 'D',
-      400   => 'CD',
-      100   => 'C',
-      90    => 'XC',
-      50    => 'L',
-      40    => 'XL',
-      10    => 'X',
-      9     => 'IX',
-      5     => 'V',
-      4     => 'IV',
-      1     => 'I'
-    }
     return '' if self.zero?
     number = self
-    romanzied = ''
-    ronan_mapping.each do |(num, roman)|
-      romanzied << (roman * (number/num))
-      number = number % num
-    end
-    return romanzied
+    roman_highest = ROMAN_MAPPING.detect {|num, roman| number >= num }
+    roman_highest[1] + (self - roman_highest[0]).to_roman
   end
 end
 
